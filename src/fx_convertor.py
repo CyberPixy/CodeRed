a656064e9c727a965294ca5714824f31630394b8
CyberPixy <w.juszczyk@interia.pl>
2024-10-30 22:26:21 +0100

    Fx_covertor correct working first draft, after fx_convertor_test workout

--- src/fx_convertor.py
+++ src/fx_convertor.py
@@ -4,23 +4,20 @@
      '''
 
 
-def swap_currency(given_currency, amount, swap_to_ccy, 
-                  ):
+def swap_currency(amount, input_ccy_code, swap_to_ccy):
     """
     Converts an amount from the given currency to the target currency
 
-    This function takes four parameters: the data with current_rates, input_ccy, the amount to swap, and the target_currency. It than calulates the converted amount based on current
+    This function takes three parameters: input_ccy, the amount to swap, and the target_currency. It than calulates the converted amount based on current
     exchange rate.
-    
-    : current_rates: dict - A dictionary containing current  fx exchange rate  source from api data
+   
     : param  given_currency: str - The currency of User input
     : param amount: float -  User amount of money give in currency 
     : param swap_to_ccy: str -  currency to buy in target currency
-    : param b
 
     :return: fload - The converted amount in the target currency
 
     """
-    converted_amount = float(amount)/fx_rates[given_currency] * fx_rates[swap_currency]
+    converted_amount = (amount/input_ccy_code) * swap_to_ccy
     return converted_amount
     
--- tests/fx_convertor_test.py
+++ tests/fx_convertor_test.py
@@ -5,25 +5,27 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 
 from fx_convertor import swap_currency
 
-
-
-
 class  TestSwapCurrency(unittest.TestCase):
 
     def setUp(self):
+        # default fx data to designed test results
         self.spot_rate = {
             'EUR': 1.0, 
             'USD': 1.08, 
             'GBP': 0.83, 
-            'JPY': 163.22
+            'JPY': 163.22,
+            'PNL': 4.34
         }
-        self.user_currency: str = "USD"
-        self.user_given_amount: float = 100
-        self.swap_currency: str = 'GBP'
     
-    def test_convert_usd_to_gbp(self):
-        result = swap_currency(self.spot_rate[self.user_currency], 100, self[swap_currency])
-        self.assertAlmostEqual(result, 76.85)
+    
+    def tearDown(self):
+        pass
+    
+    def test_convert_usd_to_gbp(self, amount:float = 100):
+        print(f'hello Wiola,Twoja kwota: {amount}')
+        print(self.spot_rate['PNL'])
+        result = swap_currency(amount, self.spot_rate['PNL'], self.spot_rate['GBP'])
+        self.assertAlmostEqual(result, 19.12, 2)
 
 if __name__ =='__main__':
     unittest.main()
